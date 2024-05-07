8349a29b301229abb83c3d0501a13bc46579bb16
Fauzan Andri <fauzanandri11@gmail.com>
2024-05-08 01:34:06 +0700

    fix arrat not found

--- src/main/java/id/my/pabeanapiaccount/config/SecurityConfiguration.java
+++ src/main/java/id/my/pabeanapiaccount/config/SecurityConfiguration.java
@@ -11,6 +11,7 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.web.cors.*;
+import java.util.Arrays;
 
 @Configuration
 @EnableWebSecurity
