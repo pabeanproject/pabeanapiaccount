1316955af5c6465b6cf33c8d0126f0c8bda921c4
Fauzan Andri <fauzanandri11@gmail.com>
2024-05-08 01:18:28 +0700

    add cors in securityconfiguration

--- src/main/java/id/my/pabeanapiaccount/config/SecurityConfiguration.java
+++ src/main/java/id/my/pabeanapiaccount/config/SecurityConfiguration.java
@@ -10,6 +10,7 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors;
 
 @Configuration
 @EnableWebSecurity
@@ -25,6 +26,7 @@ public class SecurityConfiguration {
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 
         http    
+                .cors()
                 .csrf()
                 .disable()
                 .authorizeHttpRequests()
@@ -41,4 +43,15 @@ public class SecurityConfiguration {
 
         return http.build();
     }
+
+    @Bean
+    CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(Arrays.asList("*"));
+        configuration.setAllowedMethods(Arrays.asList("*"));
+        configuration.setAllowedHeaders(Arrays.asList("*"));
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
+}
 }
